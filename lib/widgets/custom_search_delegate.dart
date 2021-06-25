64dcb1d71f595663e0387d7d57143b52a43f3028
mahdi-ko <mahdi-ko@users.noreply.github.com>
2021-06-25 17:18:26 +0300

    search finished

--- lib/widgets/custom_search_delegate.dart
+++ lib/widgets/custom_search_delegate.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomSearchDelegate extends SearchDelegate {
-  final List<Map<String, dynamic>> persons;
+  final List<Map<String, String>> persons;
+  final String imageSrc = "https://homepages.cae.wisc.edu/~ece533/images/";
   CustomSearchDelegate({required this.persons});
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
       IconButton(
         icon: Icon(Icons.close),
-        onPressed: () {},
+        onPressed: () {
+          query = '';
+        },
       )
     ];
   }
@@ -25,22 +28,56 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
+    final foundPersons = persons.where((person) {
+      return person["name"]!.toLowerCase().contains(query.toLowerCase()) ||
+              person["nickname"]!.toLowerCase().contains(query.toLowerCase())
+          ? true
+          : false;
+    }).toList();
     return ListView.builder(
-        itemCount: persons.length,
+        itemCount: foundPersons.length,
         itemBuilder: (context, index) {
+          final person = foundPersons[index];
           return ListTile(
-            leading: CircleAvatar(backgroundColor: Colors.red),
+            onTap: () {
+              query = person["name"]!;
+              Future.delayed(Duration(milliseconds: 200)).then((_) {
+                close(context, person);
+              });
+            },
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(imageSrc + person["image"]!),
+            ),
+            title: Text(person["name"]!),
+            subtitle: Text(person["nickname"]!),
           );
         });
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    final foundPersons = persons.where((person) {
+      return person["name"]!.toLowerCase().contains(query.toLowerCase()) ||
+              person["nickname"]!.toLowerCase().contains(query.toLowerCase())
+          ? true
+          : false;
+    }).toList();
     return ListView.builder(
-        itemCount: persons.length,
+        itemCount: foundPersons.length,
         itemBuilder: (context, index) {
+          final person = foundPersons[index];
           return ListTile(
-            leading: CircleAvatar(backgroundColor: Colors.red),
+            onTap: () {
+              query = person["name"]!;
+              Future.delayed(Duration(milliseconds: 200)).then((_) {
+                close(context, person);
+              });
+            },
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(imageSrc + person["image"]!),
+            ),
+            title: Text(person["name"]!),
+            subtitle: Text(person["nickname"]!),
           );
         });
   }
--- lib/widgets/search_screen.dart
+++ lib/widgets/search_screen.dart
@@ -2,22 +2,86 @@ import 'package:flutter/material.dart';
 
 import 'custom_search_delegate.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final persons = [
+    {"image": "airplane.png", "name": "Sami kali", "nickname": "The person"},
+    {"image": "arctichare.png", "name": "Ali mazen", "nickname": "The owl"},
+    {"image": "baboon.png", "name": "Sara lawski", "nickname": "Random guy"},
+    {"image": "boat.png", "name": "Mira lawyer", "nickname": "Bugs killer"},
+    {"image": "peppers.png", "name": "Moji zakara", "nickname": "Emerald"},
+    {"image": "arctichare.png", "name": "Mohammad", "nickname": "Ruby"},
+    {"image": "frymire.png", "name": "Adham", "nickname": "Diamond"},
+  ];
+
+  final List<Map<String, String>> personsToAddToList = [];
+  final String imageSrc = "https://homepages.cae.wisc.edu/~ece533/images/";
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController textFieldController = TextEditingController();
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Search"),
-          onPressed: () {
-            showSearch(
-                context: context,
-                delegate: CustomSearchDelegate(persons: [
-                  {"koko": Colors.red, "kokao": Colors.red}
-                ]));
-          },
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: textFieldController,
+                onSubmitted: (value) {
+                  showMySearch(context, textFieldController.text);
+                },
+                decoration: InputDecoration(
+                  isDense: true,
+                  border: new OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(50.0),
+                    ),
+                  ),
+                  prefixIcon: GestureDetector(
+                    onTap: () async {
+                      await showMySearch(context, textFieldController.text);
+
+                      setState(() {});
+                    },
+                    child: Icon(
+                      Icons.search,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: personsToAddToList.length,
+                itemBuilder: (context, index) {
+                  final person = personsToAddToList[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(imageSrc + person["image"]!),
+                    ),
+                    title: Text(person["name"]!),
+                    subtitle: Text(person["nickname"]!),
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Future<void> showMySearch(BuildContext context, String query) async {
+    final Map<String, String> foundPerson = await showSearch(
+        context: context, delegate: CustomSearchDelegate(persons: persons), query: query);
+    setState(() {
+      personsToAddToList.add(foundPerson);
+    });
   }
 }
